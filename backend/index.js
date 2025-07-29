020488136ea79e451ec7bf0152e97e9e12c98980
aryan-valmiki <aryanvalmiki780@gmail.com>
2025-07-29 12:59:56 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ const port = process.env.PORT || 5000;
 app.use(cookieParser());
 app.use(express.json());
 app.use(cors({
-  origin: "http://localhost:5173", 
+  origin: "https://appverse-ecommerce-frontend.onrender.com", 
   credentials: true               
 }));
 
